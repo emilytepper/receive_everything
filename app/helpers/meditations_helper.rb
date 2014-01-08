9deec1d71460e9973449c00f4da90950933f6cba
Mike Schwab <mike.schwab@gmail.com>
2014-01-08 08:52:17 -0500

    admin can listen to meditations via soundcloud embed

--- app/helpers/meditations_helper.rb
+++ app/helpers/meditations_helper.rb
@@ -1,2 +1,5 @@
 module MeditationsHelper
+  def soundcloud_embed_for_meditation meditation
+    %Q!<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=#{meditation.soundcloud_url}&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>!.html_safe
+  end
 end
