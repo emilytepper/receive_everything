681265eef40c2c422633f52a86b7d63db71fb50a
Mike Schwab <mike.schwab@gmail.com>
2014-01-08 08:41:49 -0500

    when adding meditations to a product, only meditations not already included are options

--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -1,4 +1,6 @@
 class Meditation < ActiveRecord::Base
   has_many :inclusions
   has_many :products, :through => :inclusions
+  
+  scope :not_in_product, lambda { |product| product.inclusions.any? ? where('id not in (?)', product.inclusions.map(&:meditation_id)) : nil }
 end
