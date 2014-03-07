93bfc9ece582dd8f8c7c6ad21074ee07d0dcc6be
Mike Schwab <mike.schwab@gmail.com>
2014-03-06 21:44:21 -0500

    free products accessible after login

--- app/models/mystic.rb
+++ app/models/mystic.rb
@@ -3,6 +3,8 @@ class Mystic < ActiveRecord::Base
   has_many :meditations, :through => :accesses
   has_many :purchases
 
+  after_create :gain_access_to_free_meditations
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
   def has_access_to? meditation
@@ -10,10 +12,16 @@ class Mystic < ActiveRecord::Base
   end
   
   def has_access_to_product? product
-    purchases.map(&:product).include? product
+    purchases.map(&:product).include?(product) or product.meditations.all? { |meditation| has_access_to? meditation }
   end
   
   def password_required?
     super unless facebook_id.present?
+  end
+  
+  def gain_access_to_free_meditations
+    Meditation.free.each do |meditation|
+      accesses.create :meditation => meditation
+    end
   end
 end
