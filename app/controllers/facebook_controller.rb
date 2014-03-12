78f93c177ae794542d1fba3a327d2b30b6008995
Mike Schwab <mike.schwab@gmail.com>
2014-03-11 20:49:20 -0400

    save first/last name from facebook and debug missing redirect to goddess sign up page

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -6,10 +6,16 @@ class FacebookController < ApplicationController
         sign_in @mystic
       elsif @mystic = Mystic.find_by_email(current_facebook_user.email)
         @mystic.update_attribute(:facebook_id, current_facebook_user.id)
+        if @mystic.first_name.blank?
+          @mystic.update_attribute :first_name, current_facebook_user.first_name
+        end
+        if @mystic.last_name.blank?
+          @mystic.update_attribute :last_name, current_facebook_user.last_name
+        end
         @mystic.confirm!
         sign_in @mystic
       else
-        @mystic = Mystic.new :email => current_facebook_user.email, :facebook_id => current_facebook_user.id
+        @mystic = Mystic.new :email => current_facebook_user.email, :facebook_id => current_facebook_user.id, :first_name => current_facebook_user.first_name, :last_name => current_facebook_user.last_name
         @mystic.skip_confirmation!
         @mystic.save
         sign_in @mystic
@@ -18,6 +24,7 @@ class FacebookController < ApplicationController
     if URI(request.referrer).path == 'goddesses/new'
       redirect_to new_goddess_path
     else
+      Rails.logger.info "request.referrer = #{URI(request.referrer)}"
       redirect_to root_url
     end
   end
