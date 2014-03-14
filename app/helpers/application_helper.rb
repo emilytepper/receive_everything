ff17cca64513fb6b297e8f01e520bce8fa153081
Mike Schwab <mike.schwab@gmail.com>
2014-03-14 16:22:55 -0400

    creatrix can grant mystics access to meditations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
-  def link_to_name record
+  def link_to_name record, mthd = nil
     return '' unless record
-    if record.respond_to?(:name)
-      link_to record.name, record
+    if mthd
+      if record.respond_to?(mthd)
+        link_to record.send(mthd), record
+      else
+        link_to record.id, record
+      end
     else
-      link_to record.id, record
+      if record.respond_to?(:name)
+        link_to record.name, record
+      else
+        link_to record.id, record
+      end
     end
   end
 end
